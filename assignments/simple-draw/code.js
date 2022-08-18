bf33d1cd9e97a08eaff73e45c1eed9556d7918d2
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-08-18 15:10:25 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -43,4 +43,6 @@ drawRect(50, 50, 60, 40, 'black');
 
 drawFilledCircle(width - 250, height - 300, 50, 'black');
 
-drawFilledRect(10, height - 40, 40, 10, 'purple');
+drawFilledRect(10, 40, 40, 10, 'purple');
+
+
