3c1febea25b3ef3bac10f49e3d4670c1514ccf42
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-09-06 15:21:52 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,3 +1,21 @@
 const add = (a,b) => {
   return a+b;
 };
+const subtract = (a,b) => {
+  return a-b;
+};
+const multiply = (a,b) => {
+  return a*b;
+};
+const divide = (a,b) => {
+  return a/b;
+};
+const mod = (a,b) => {
+  return a%b;
+};
+const averageOfTwo = (a,b) => {
+  return (a+b)/2;
+};
+const averageOfThree = (a,b) => {
+  return (a+b+c)/3;
+}; 
