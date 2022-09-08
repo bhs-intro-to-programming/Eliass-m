2b6b13a92cb4825d145f8658b78af7d05d1ba4dc
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-09-08 15:20:30 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -34,3 +34,6 @@ const eatsWorms = (FlobbyBirds, BloggyBirds, FlibbleBirds, GlobbyBirds) => {
 const eatsNuts = (red, spotted) => {
   return red ||! spotted
 }
+const eatsNuts = (red, spotted) => {
+  return red || spotted
+}
