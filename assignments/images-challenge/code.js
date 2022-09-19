77e0073248a6a5c4b2fe4eec8029398cdec85933
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-09-19 15:07:09 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -25,6 +25,6 @@
  */
 
 const lineOfCircles = () => {
-for (let x = 0; x < 20; x++)
-drawFilledCircle(10, 300, 10, 'red', 2)
+for (let x = 0; x < 20; x++){
+drawFilledCircle(10, 300, 10, 'red', 2)}
 }
