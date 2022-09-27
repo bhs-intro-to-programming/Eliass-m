ff030d5320f3572899a151eaaae7c85cf25431d9
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-09-27 14:58:23 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -29,7 +29,7 @@ const fillwithcircles = (r) => {
   const d = r * 2
   for (let x = 0; x < numCircles; x++)
     if (x > length) {
-      drawFilledCircle(x * r * 1.5, 300, 15, 'red')
+      drawFilledCircle(x * 30, 300, 15, 'red')
     }
 }
 fillwithcircles(15);
