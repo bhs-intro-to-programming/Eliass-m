53b32597e05d1af70cbc14e65f2839e1326f2eef
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-09-27 14:57:26 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -32,7 +32,7 @@ const fillwithcircles = (r) => {
       drawFilledCircle(x * r * 1.5, 300, 15, 'red')
     }
 }
-fillwithcircles(18);
+fillwithcircles(20);
 
 //const everyOtherCircle = () => {
 //return (i=='blue',o=='red')
