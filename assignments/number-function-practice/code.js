472bf895d7c1329ee40f7d739f17d036a80b2726
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-10-05 15:03:12 -0700

    Updating assignments/number-function-practice/code.js

--- assignments/number-function-practice/code.js
+++ assignments/number-function-practice/code.js
@@ -1,3 +1,33 @@
+const plusone = (s) => {
+  return s + 1
+}
+const plustwo = (s) => {
+  return s + 2
+}
+const plusthree = (s) => {
+  return s + 3
+}
+const plusfour = (s) => {
+  return s + 4
+}
+const plusfive = (s) => {
+  return s + 5
+}
+const plussix = (s) => {
+  return s + 6
+}
+const plusseven = (s) => {
+  return s + 7
+}
+const pluseight = (s) => {
+  return s + 8
+}
+const plusnine = (s) => {
+  return s + 9
+}
+const plusten = (s) => {
+  return s + 10
+}
 const timesone = (s) => {
   return s * 1
 }
@@ -27,4 +57,64 @@ const timesnine = (s) => {
 }
 const timesten = (s) => {
   return s * 10
+}
+const minusone = (s) => {
+  return s - 1
+}
+const minustwo = (s) => {
+  return s - 2
+}
+const minusthree = (s) => {
+  return s - 3
+}
+const minusfour = (s) => {
+  return s - 4
+}
+const minusfive = (s) => {
+  return s - 5
+}
+const minussix = (s) => {
+  return s - 6
+}
+const minusseven = (s) => {
+  return s - 7
+}
+const minuseight = (s) => {
+  return s - 8
+}
+const minusnine = (s) => {
+  return s - 9
+}
+const minusten = (s) => {
+  return s - 10
+}
+const dividedbyone = (s) => {
+  return s / 1
+}
+const dividedbytwo = (s) => {
+  return s / 2
+}
+const dividedbythree = (s) => {
+  return s / 3
+}
+const dividedbyfour = (s) => {
+  return s / 4
+}
+const dividedbyfive = (s) => {
+  return s / 5
+}
+const dividedbysix = (s) => {
+  return s / 6
+}
+const dividedbyseven = (s) => {
+  return s / 7
+}
+const dividedbyeight = (s) => {
+  return s / 8
+}
+const dividedbynine = (s) => {
+  return s / 9
+}
+const dividedbyten = (s) => {
+  return s / 10
 }
