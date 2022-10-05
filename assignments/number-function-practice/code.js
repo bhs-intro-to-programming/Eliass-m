fc723594b8d3bb73db0d57f1134331aabb30f8b2
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-10-05 14:53:14 -0700

    Updating assignments/number-function-practice/code.js

--- assignments/number-function-practice/code.js
+++ assignments/number-function-practice/code.js
@@ -1,3 +1,30 @@
+const timesone = (s) => {
+  return s * 1
+}
+const timestwo = (s) => {
+  return s * 2
+}
+const timesthree = (s) => {
+  return s * 3
+}
+const timesfour = (s) => {
+  return s * 4
+}
+const timesfive = (s) => {
+  return s * 5
+}
+const timessix = (s) => {
+  return s * 6
+}
 const timesseven = (s) => {
-  return s*7
+  return s * 7
+}
+const timeseight = (s) => {
+  return s * 8
+}
+const timesnine = (s) => {
+  return s * 9
+}
+const timesten = (s) => {
+  return s * 10
 }
