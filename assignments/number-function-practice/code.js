3228fbb59b68bc4ef032e8225125be81f713ed9a
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-10-08 18:39:10 -0700

    Updating assignments/number-function-practice/code.js

--- assignments/number-function-practice/code.js
+++ assignments/number-function-practice/code.js
@@ -1,120 +1,39 @@
-const plusone = (s) => {
-  return s + 1
-}
-const plustwo = (s) => {
-  return s + 2
-}
-const plusthree = (s) => {
-  return s + 3
-}
-const plusfour = (s) => {
-  return s + 4
-}
-const plusfive = (s) => {
-  return s + 5
-}
-const plussix = (s) => {
-  return s + 6
-}
-const plusseven = (s) => {
-  return s + 7
-}
-const pluseight = (s) => {
-  return s + 8
-}
-const plusnine = (s) => {
-  return s + 9
-}
-const plusten = (s) => {
-  return s + 10
-}
-const timesone = (s) => {
-  return s * 1
-}
-const timestwo = (s) => {
-  return s * 2
-}
-const timesthree = (s) => {
-  return s * 3
-}
-const timesfour = (s) => {
-  return s * 4
-}
-const timesfive = (s) => {
-  return s * 5
-}
-const timessix = (s) => {
-  return s * 6
-}
-const timesseven = (s) => {
-  return s * 7
-}
-const timeseight = (s) => {
-  return s * 8
-}
-const timesnine = (s) => {
-  return s * 9
-}
-const timesten = (s) => {
-  return s * 10
-}
-const minusone = (s) => {
-  return s - 1
-}
-const minustwo = (s) => {
-  return s - 2
-}
-const minusthree = (s) => {
-  return s - 3
-}
-const minusfour = (s) => {
-  return s - 4
-}
-const minusfive = (s) => {
-  return s - 5
-}
-const minussix = (s) => {
-  return s - 6
-}
-const minusseven = (s) => {
-  return s - 7
-}
-const minuseight = (s) => {
-  return s - 8
-}
-const minusnine = (s) => {
-  return s - 9
-}
-const minusten = (s) => {
-  return s - 10
-}
-const dividedbyone = (s) => {
-  return s / 1
-}
-const dividedbytwo = (s) => {
-  return s / 2
-}
-const dividedbythree = (s) => {
-  return s / 3
-}
-const dividedbyfour = (s) => {
-  return s / 4
-}
-const dividedbyfive = (s) => {
-  return s / 5
-}
-const dividedbysix = (s) => {
-  return s / 6
-}
-const dividedbyseven = (s) => {
-  return s / 7
-}
-const dividedbyeight = (s) => {
-  return s / 8
-}
-const dividedbynine = (s) => {
-  return s / 9
-}
-const dividedbyten = (s) => {
-  return s / 10
-}
+const itemsLeftOver = (numppl, numitems) => {
+  return;
+};
+
+const areaOfCircle = (r) => {
+  return (Math.PI)(r ** 2);
+};
+
+const volumeOfCube = (length) => {
+  return length ** 3;
+};
+
+const populationGrowth = (popsize, growthrate) => {
+  return popsize * growthrate;
+};
+
+const earnedRunAverage = (earnedruns, innings) => {
+  return (earnedruns / innings) * 9;
+};
+
+const valueOfJewels = (numdiamonds, numemeralds, goldvaldiamond, goldvalemerald) => {
+  return (numdiamonds * goldvaldiamond) + (numemeralds * goldvalemerald);
+};
+
+const payWithOvertime = () => {
+  ;
+};
+
+const firstClassPostage = () => {
+  ;
+};
+
+const weightOnJupiter = () => {
+  ;
+};
+
+const gravity = () => {
+  ;
+};
