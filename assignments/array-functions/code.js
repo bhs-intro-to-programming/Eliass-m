ec3eaff92ff1f826a7204137710bed50530a7c84
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-10-16 22:21:43 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -2,11 +2,9 @@ const countTens = (arrayTens) => {
   let tens = 0
   for (let i = 0; i < arrayTens.length; i++){
     console.log(arrayTens[i])
-    let j = arrayTens[i]
-    if (j == 10){
+    if (arrayTens == 10){
       console.log(j)
       tens++
-    
     }
   }
   return tens
