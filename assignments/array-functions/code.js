255a0e15dccfd70f5f1ea2b74cbe0f652d09b13d
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-10-16 19:28:32 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,7 +1,7 @@
 const countTens = ['1', '2', '3', '4', '5'];
 
-countTens.forEach ('10') => console.log(1);
+//countTens.forEach ('10') => console.log(1);
 
 
 
-const anyOverOneHundred = 
+//const anyOverOneHundred = 
