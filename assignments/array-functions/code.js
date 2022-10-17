23b22798f9307f4737732c7385a294f9cc2fd34d
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-10-16 22:51:12 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -9,7 +9,16 @@ const countTens = (arrayTens) => {
   return tens
 };
 
-
+//const sum = (arraysum) => {
+//  let tens = 0
+//  for (let i = 0; i < arrayTens.length; i++){
+//    console.log(arrayTens[i])
+//    if (arrayTens[i] == 10){
+//    tens++
+//    }
+//  }
+//  return tens
+//};
 
 
 //const anyOverOneHundred = 
