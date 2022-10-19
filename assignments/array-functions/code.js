e9a955eef47dfa1ef9de9dcb428762fa2d57bfc6
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-10-19 15:19:33 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -29,9 +29,9 @@ const anyOverOneHundred = (array) => {
   let num = 0
   for (const element of array) {
     if (element > 100) {
+    num++;
     }
   }
-
   if (num > 0) {
     return true
   } else {
