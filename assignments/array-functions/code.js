0d95a7fdf247d8fd432f5be9836c562d7d6fcbb0
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-10-20 14:35:43 -0700

    Updating assignments/array-functions/code.js

---
+++ assignments/array-functions/code.js
@@ -0,0 +1,47 @@
+const countTens = (array) => {
+  let tens = 0
+  for (const element of array) {
+    if (array[i] == 10) {
+      tens++
+    }
+  }
+  return tens
+};
+
+const sum = (array) => {
+  let add = 0
+  for (const element of array) {
+    add += element;
+  }
+  return add
+}
+
+const evens = (array) => {
+  let evenArray = []
+  for (const element of array) {
+    if (element % 2 === 0)
+      evenArray.push(element)
+  }
+  return evenArray
+}
+
+const anyOverOneHundred = (array) => {
+  let num = 0
+  for (const element of array) {
+    if (element > 100) {
+    num++;
+    }
+  }
+  if (num > 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+const pyramid = (array) => {
+  const stack = []
+  for (let) 
+
+}
