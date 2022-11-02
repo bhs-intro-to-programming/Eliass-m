20b080b8af1480fde641d11bd236bb3075425cc2
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-02 15:27:13 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -5,3 +5,31 @@
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
 let turn = "X";
+registerOnclick((x, y) => {
+  drawText(turn, x - 50, y + 50, 'black', Math.min(width, height) * 0.3);
+  turn = turn === "X" ? turn = "O" : turn = "X";
+});
+
+const drawBoard = () => {
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * width / 3
+    drawLine(x, 0, x, height, 'black', 3)
+  }
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * height / 3
+    drawLine(0, x, width, x, 'black', 3)
+  }
+}
+drawBoard()
+
+const yRow = (height) => {
+  let rowHeight = height / 3;
+  Math.floor (y/rowHeight)
+}
+
+const xRow = (width) => {
+  let rowWidth = width / 3;
+  Math.floor (x/rowWidth)
+}
+
+
