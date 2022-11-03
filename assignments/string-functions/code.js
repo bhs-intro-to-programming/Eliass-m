71ddaea9aec1111e98dc0c8c2dd2724d70321b67
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-02 18:19:13 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -12,3 +12,7 @@ const stringContains = (s1, s2) => {
 const firstAndLast = (s) => {
   return s[0] + s[s.length-1]
 }
+
+const swapFrontAndBack = (s) => {
+  return s.substring(s.length/2, s.length) + s.substring(0, s.length/2)
+}
