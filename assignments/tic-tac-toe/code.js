57a0b036f6731284d8153e5f2bcd1e3a10e01a71
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-04 15:24:33 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -4,7 +4,39 @@
 // of the equals sign in our normal `const foo = ...` function definition. This
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
-
+let turn = "X";
 registerOnclick((x, y) => {
-  drawText('X', x, y, 'black', Math.min(width, height) * 0.3);
+
+  const spacingHeight = height/3
+  const yBoxSpace = Math.floor (y / spacingHeight) 
+  
+  const spacingWidth = width/3
+  const xBoxSpace = Math.floor (x / spacingWidth)       
+  
+  drawText(turn, (xBoxSpace * (spacingWidth) + spacing / 2) - 50, (yBoxSpace * (spacingWidth) + spacing / 2) + 50, 'black', Math.min(width, height) * 0.3);
+  turn = turn === "X" ? turn = "O" : turn = "X";
 });
+
+const drawBoard = () => {
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * width / 3
+    drawLine(x, 0, x, height, 'black', 3)
+  }
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * height / 3
+    drawLine(0, x, width, x, 'black', 3)
+  }
+}
+drawBoard()
+
+const yRow = (height) => {
+  let rowHeight = height / 3;
+  Math.floor (y/rowHeight)
+}
+
+const xRow = (width) => {
+  let rowWidth = width / 3;
+  Math.floor (x/rowWidth)
+}
+
+
