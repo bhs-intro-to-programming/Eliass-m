ff75ae3099ee3951e9412fcacef36511a3aa67ec
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-07 15:55:48 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -2,8 +2,8 @@ const getX = (o) => {
   return o.x
 }
 
-const point = (x , y) => {
-  return {"x" : x, "y" : y}
+const point = (x, y) => {
+  return { "x": x, "y": y }
 }
 
 const emptyObject = () => {
@@ -11,9 +11,17 @@ const emptyObject = () => {
 }
 
 const distance = (o1, o2) => {
-  return Math.sqrt ((o1.x - o2.x)**2  + (o1.y - o2.y)**2)
+  return Math.sqrt((o1.x - o2.x) ** 2 + (o1.y - o2.y) ** 2)
 }
 
 const midpoint = (o1, o2) => {
-  return {"x": (o1.x + o2.x)/2 , "y": (o1.y + o2.y)/2}
+  return { "x": (o1.x + o2.x) / 2, "y": (o1.y + o2.y) / 2 }
+}
+
+const sumSalaries = (array) => {
+  let sum = 0
+  for (let i = 0; i < array.length){
+    sum += array[i].salary
+  }
+  return sum
 }
