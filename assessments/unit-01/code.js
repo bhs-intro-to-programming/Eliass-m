f59817dc800d1bcf10a57bc612446b9c73a1fcdc
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-09 14:58:18 -0800

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -1,7 +1,27 @@
 // You will use this function in one of the problems. You don't need to change
 // it or even worry about how exactly it works.
-const emit = (a, b, product) => {
-  console.log(`${a} times ${b} is ${product}`);
+
+//const timesTable
+//const emit = (a, b, product) => {
+//  console.log(`${a} times ${b} is ${product}`);
+//};
+
+const averageWeight = (weight, numitems) => {
+  return weight / numitems;
 };
 
-// Write your code here ...
+const hypotenuse = (sidea, sideb) => {
+  return Math.sqrt(sidea ** 2 + sideb ** 2);
+};
+
+const maxRadius = (width, height) => {
+  return Math.min(width, height) /2
+};
+
+const numCircles = (radius, width) => {
+  return Math.floor(width / (radius *2))
+}
+
+const offset = (areawidth, figurewidth) => {
+  
+}
