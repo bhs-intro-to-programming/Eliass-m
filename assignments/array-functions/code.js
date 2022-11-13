643ec8704e40fbe3eae009b86dc47555e0a12758
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-12 19:17:56 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,37 +1,43 @@
-const countTens = (numArr) => {
+const countTens = (array) => {
   let tens = 0
-  for (const element of numArr){
-    if (element === 10){
-      tens ++
+  for (const element of array) {
+    if (element === 10) {
+      tens++
     }
-  } return tens
-}
-
-const sum = (numArr) => {
-  let total = 0
-  for (const element of numArr){
-    total += element
-  } return total
-}
-
-const evens = (numArr) => {
-  let even = []
-  for (const element of numArr){
-    if (element % 2 === 0){
-    even.push(element)
-    }
-  } return even
-}
-
-const anyOverOneHundred = (numArr) => {
-  let n = 0
-  for (const element of numArr){
-  if (element > 100){
-    n++
-  } 
-  if (n > 0){
-  return true
   }
-  else return false
-  } 
+  return tens
+};
+
+const sum = (array) => {
+  let add = 0
+  for (const element of array) {
+    add += element;
+  }
+  return add
 }
+
+const evens = (array) => {
+  let evenArray = []
+  for (const element of array) {
+    if (element % 2 === 0)
+      evenArray.push(element)
+  }
+  return evenArray
+}
+
+const anyOverOneHundred = (array) => {
+  let num = 0
+  for (const element of array) {
+    if (element > 100) {
+    num++;
+    }
+  }
+  if (num > 0) {
+    return true
+  } else return false
+  
+}
+
+
+
+
