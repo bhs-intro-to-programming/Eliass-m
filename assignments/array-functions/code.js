c474988a9af60a2d43161217ce1fb29854605cf5
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-12 19:33:15 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -42,6 +42,7 @@ const anyOverOneHundred = (array) => {
 const pyramid = (num) => {
   let array = []
   for (let i = 0; i <= num; i++){
+    for (let j = 0; j < i; j++)
       array.push(i)
     }
   return array
