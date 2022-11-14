ffcc8cb5373e3da7eaac365464ce8bfd5e4d6c11
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-14 15:34:18 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -2,61 +2,31 @@
 // should save you some time.
 
 const area = (rect) => {
-return rect.width * rect.height
 };
 
 const higherPaid = (e1, e2) => {
-const es1 = e1.salary 
-const es2 = e2.salary
-if (es1 > es2){
-  return e1
-}
-else return e2
 };
 
 const isSamePoint = (p1, p2) => {
-if (p1.x === p2.x) {
-  if (p1.y === p2.y){
-    return true
-  } else return false
-} else return false
 };
 
 const totalWithTip = (bill, tipPercentage) => {
-return {"subtotal" : bill.subtotal, "tip" : bill.subtotal * tipPercentage, "total" : bill.subtotal + (bill.subtotal * tipPercentage) }
 };
 
 const isWinner = (player) => {
-if (player.score > 100){
-  return true
-} else return false
 };
 
 const updateWins = (players) => {
-for (const player of players) {
-  if (player.score > 100){
-    player.wins++
-  }
-}
 };
 
 const bigWinners = (players) => {
-let newArr = []
-for (const player of players){
-  if (player.wins > 10){
-    newArr.push(player)
-  }
-} return newArr
 };
 
 const fillTimesTable = (table) => {
-
 };
 
 const sums = (n) => {
-
 };
 
 const rule110 = (cells) => {
-
 };
