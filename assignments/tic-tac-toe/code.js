03b5f1f3840a24a5a97bf92fae3e61c044e1dc20
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-18 15:32:00 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -4,7 +4,46 @@
 // of the equals sign in our normal `const foo = ...` function definition. This
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
+const cordArray = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+]
 let turn = "X";
+
+const drawBoard = () => {
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * width / 3
+    drawLine(x, 0, x, height, 'black', 3)
+  }
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * height / 3
+    drawLine(0, x, width, x, 'black', 3)
+  }
+}
+drawBoard()
+
+
+const won = (turn) => {
+  if (cordArray[0][0] === turn && cordArray[0][1] === turn && [0][2] === turn) {
+    winner = { winner: turn, winType: 'horizontal', winLocate: 'top' }
+  } else if (cordArray[1][0] === turn && cordArray[1][1] === turn && [1][2] === turn) {
+    winner = { winner: turn, winType: 'horizontal', winLocate: 'mid' }
+  } else if (cordArray[2][0] === turn && cordArray[2][1] === turn && [2][2] === turn) {
+    winner = { winner: turn, winType: 'horizontal', winLocate: 'low' }
+  } else if (cordArray[0][0] === turn && cordArray[1][0] === turn && [2][0] === turn) {
+    winner = { winner: turn, winType: 'vertical', winLocate: 'left' }
+  } else if (cordArray[0][1] === turn && cordArray[1][1] === turn && [2][1] === turn) {
+    winner = { winner: turn, winType: 'vertical', winLocate: 'mid' }
+  } else if (cordArray[0][2] === turn && cordArray[1][2] === turn && [2][2] === turn) {
+    winner = { winner: turn, winType: 'vertical', winLocate: 'right' }
+  } else if (cordArray[0][0] === turn && cordArray[1][1] === turn && [2][2] === turn) {
+    winner = { winner: turn, winType: 'diagonal', winLocate: 'lToR' }
+  } else if (cordArray[0][2] === turn && cordArray[1][1] === turn && [2][0] === turn) {
+    winner = { winner: turn, winType: 'diagonal', winLocate: 'rToL' }
+  }
+  return winner
+}
 
 registerOnclick((x, y) => {
 
@@ -18,17 +57,7 @@ registerOnclick((x, y) => {
 
 });
 
-const drawBoard = () => {
-  for (let i = 0; i < 2; i++) {
-    const x = (i + 1) * width / 3
-    drawLine(x, 0, x, height, 'black', 3)
-  }
-  for (let i = 0; i < 2; i++) {
-    const x = (i + 1) * height / 3
-    drawLine(0, x, width, x, 'black', 3)
-  }
-}
-drawBoard()
+
 
 const yRow = (height) => {
   let rowHeight = height / 3;
