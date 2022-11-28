0e8c372c89d613ff74de0615b253c219bbd9ef5a
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-28 15:25:59 -0800

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -29,3 +29,7 @@ const averageOfThree = (n1, n2, n3) => {
 const distance = (n1, n2) => {
   return Math.abs(n1 - n2)
 }
+
+const manhattanDistance = (x1, y1, x2, y2) => {
+  return Math.abs(x1 - x2) + Math.abs(y1 - y2)
+}
