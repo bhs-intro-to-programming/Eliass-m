114a475faf50217786234c79435a61e2a1dabf5f
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-28 15:17:09 -0800

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -5,3 +5,11 @@ const add = (n1, n2) => {
 const subtract = (n1, n2) => {
   return n1 - n2
 }
+
+const multiply = (n1, n2) => {
+  return n1 * n2
+}
+
+const divide = (n1, n2) => {
+  return n1 / n2
+}
