f7a36fbeedbb6cb6e84b0d2466842d58f9cfb049
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-11-29 14:43:14 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -5,3 +5,7 @@ const totalEggs = (hardB, softB) => {
 const chocolatesPerPerson = (numCh, numP) => {
   return (numCh - (numCh % numP))/numP
 }
+
+const extraChocolates = (numCh, numP) => {
+  return numCh % numP
+}
