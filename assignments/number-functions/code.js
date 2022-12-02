16aa8e9d1742b383785f61debd78af94fae48b68
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-12-01 20:07:12 -0800

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -6,12 +6,12 @@ const subtract = (n1, n2) => {
   return n1 - n2
 }
 
-const multiply = (n1, n2 => {
-  return n1 * n2 
+const multiply = (n1, n2) => {
+  return n1 * n2
 }
 
 const divide = (n1, n2) => {
-  return n1 / n2;
+  return n1 / n2
 }
 
 const mod = (n1, n2) => {
@@ -34,4 +34,3 @@ const manhattanDistance = (x1, y1, x2, y2) => {
   return Math.abs(x1 - x2) + Math.abs(y1 - y2)
 }
 
-const euclideanDistance = (x1, y1)
