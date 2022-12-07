be0c435648e895e28afa2308219b38ab5e38613a
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-12-07 15:27:39 -0800

    Updating assessments/2022-12-07-control-constructs/code.js

--- assessments/2022-12-07-control-constructs/code.js
+++ assessments/2022-12-07-control-constructs/code.js
@@ -116,7 +116,7 @@ const threewayClassify = (value) => {
 
 const sumOfSquares = (n) => {
   let sum = 0
-  for (i = 0; i < n; i++){
+  for (let i = 0; i < n; i++){
     sum += i**2
   }
   return sum
