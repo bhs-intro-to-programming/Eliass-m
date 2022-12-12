0d6a2dc48ea7bd6cf3c1747cf592b6bc75efadb1
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-12-12 15:13:22 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -11,3 +11,11 @@ const makeBoard = () => {
 const makeMove = (mark, row, column) => {
   return {'mark' : mark, 'row' : row, 'column' : column}
 }
+
+const placeMark = () => {
+
+}
+
+const allTheSame = (array) => {
+  return array[0] === array[1] === array[2]
+}
