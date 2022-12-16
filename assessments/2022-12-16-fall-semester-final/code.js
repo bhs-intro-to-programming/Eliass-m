a3bd8526f473681c13c11d24b178acbb116a584f
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-12-16 11:31:11 -0800

    Updating assessments/2022-12-16-fall-semester-final/code.js

--- assessments/2022-12-16-fall-semester-final/code.js
+++ assessments/2022-12-16-fall-semester-final/code.js
@@ -134,7 +134,7 @@ const allTheSame = (array) => {
 const extractColumn = (array, index) => {
   let newArr = []
   for (let i = 0; i < array.length; i++){
-    newArr.push([i],[index])
+    newArr.push([i][index])
   }
   return newArr
 }
