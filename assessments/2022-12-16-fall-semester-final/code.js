64792e7d2177e7b0fbc9b626c53828d20f2cef06
Eliass-m <111394714+Eliass-m@users.noreply.github.com>
2022-12-16 11:09:18 -0800

    Updating assessments/2022-12-16-fall-semester-final/code.js

--- assessments/2022-12-16-fall-semester-final/code.js
+++ assessments/2022-12-16-fall-semester-final/code.js
@@ -116,3 +116,7 @@ const maxRadius = (width, height) => {
 const numCircles = (radius, width) => {
   return Math.floor(width/(radius*2))
 }
+
+const allTheSame = (array) => {
+  return array[0] === array[1] && array[0] === array[2]
+}
